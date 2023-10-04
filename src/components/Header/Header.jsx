5bf243cb1f0ca3884b20a9e26a9d1a43b5d33c2c
Anton Kostylev <forestpk13@gmail.com>
2023-10-04 23:22:01 +0700

    current user context added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import Navbar from '../Navbar/Navbar';
 import './Header.css';
 
 function Header() {
+  const currentUser = useContext(CurrentUserContext);
   let location = useLocation();
   const [isMainPageHeader, setIsMainPageHeader] = useState(false);
   const [isBurgerOpened, setIsBurgerOpened] = useState(true);
@@ -48,32 +50,39 @@ function Header() {
         to='/'
         title='На главную'
         />
-      <button
-          className={`header__burger ${isBurgerOpened ? 'header__burger_opened' : ''}`}
-          type='button'
-          onClick={openBurger}
-      />
-      <Navbar isVisible={isBurgerOpened} onClose={closeBurger}/>
-      <nav className='header__auth'>
-        <ul className='header__auth-links'>
-          <li>
-            <Link
-              className='header__auth-link'
-              to='/signup'
-              >
-                Регистрация
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='header__auth-link header__auth-link_marked'
-              to='/signin'
-              >
-                Войти
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      {currentUser &&
+        <button
+            className={`header__burger ${isBurgerOpened ? 'header__burger_opened' : ''}`}
+            type='button'
+            onClick={openBurger}
+        />
+      }
+      {
+        currentUser
+          ? <Navbar isVisible={isBurgerOpened} onClose={closeBurger}/>
+          : (
+            <nav className='header__auth'>
+              <ul className='header__auth-links'>
+                <li>
+                  <Link
+                    className='header__auth-link'
+                    to='/signup'
+                  >
+                    Регистрация
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className='header__auth-link header__auth-link_marked'
+                    to='/signin'
+                  >
+                    Войти
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          )
+      }
     </header>
   )
 };
