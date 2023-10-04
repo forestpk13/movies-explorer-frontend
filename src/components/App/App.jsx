c09b9960e0e7b2eb9a9ab058e69b075e52c9239a
Anton Kostylev <forestpk13@gmail.com>
2023-10-05 00:01:30 +0700

    pageNotFound component added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Landing from '../Landing/Landing';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import {
+  pagesWithHeader,
+  pagesWithFooter
+} from '../../utils/constants'
 
 import './App.css';
 
 function App() {
+  const location = useLocation().pathname;
+
+  const isPageWithHeader = pagesWithHeader.includes(location);
+  const isPageWithFooter = pagesWithFooter.includes(location);
+
   return (
       <div className="page">
           <>
-            <Header />
+            {isPageWithHeader && <Header />}
             <Routes>
               <Route path='/' element={<Landing />} />
+              <Route path='*' element={<PageNotFound />} />
             </Routes>
-            <Footer />
+            {isPageWithFooter && <Footer />}
           </>
       </div>
   );
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const pagesWithHeader = ['/', '/movies', '/saved-movies', '/profile'];
+export const pagesWithFooter = ['/', '/movies', '/saved-movies'];
